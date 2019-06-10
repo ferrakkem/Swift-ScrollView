adfb6bd8b99c0ca16cb393832156ddfdc4a83037
ferrakkem <ferrakkem.bhuiyan@btraccl.com>
2019-06-10 12:45:14 +0600

    Update background color

--- ScrollView/ViewController.swift
+++ ScrollView/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    var images = [UIImageView]()
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
     }
-
+    
+    override func viewWillAppear(_ animated: Bool)
+    {
+        var contentWidth:CGFloat = 0.0
+        let scrollWidth = scrollView.frame.size.width
+        
+        for x in 0...2
+        {
+            let image = UIImage(named: "icon\(x).png")
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            var newX: CGFloat = 0.0
+            newX = scrollWidth / 2 + scrollWidth * CGFloat(x)
+            contentWidth = contentWidth + newX
+            scrollView.addSubview(imageView)
+            
+            imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height / 2) - 75, width: 150, height: 150)
+        }
+        scrollView.clipsToBounds = false
+        scrollView.backgroundColor =UIColor.white
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
+        
+    }
 
 }
 
